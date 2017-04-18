2731a0e502fd183f849abd19f386e59db035141a
Chloe <chenderson126+bitbucket@gmail.com>
2017-04-18 14:06:51 -0500

    Color-coded chart finished.

--- public/script.js
+++ public/script.js
@@ -2,6 +2,18 @@ $(function() {
     var peerReviewCanvas = $('#peer-review')[0];
     // context
     var peerReviewCtx = peerReviewCanvas.getContext('2d'); // treat it as a 2-D canvas (x- and y-axis)
+    var colors = [
+        'red',
+        'orange',
+        'green',
+        'blue',
+        'purple',
+        'indigo',
+        'black',
+        'maroon',
+        'chartreuse',
+        'violet'
+    ];
 
     // Draw peer review chart
     peerReviewCtx.strokeText("Peer Review", 70, 10); // 'Peer Review' at (10, 10)
@@ -9,20 +21,16 @@ $(function() {
     for (var i = 0; i <= 10; i++) {
         peerReviewCtx.fillText(10 - i, 10, 30 + 20 * i);
         peerReviewCtx.moveTo(25, 30 + 20 * i);
-        peerReviewCtx.lineTo(200, 30 + 20 * i);
+        peerReviewCtx.lineTo(90, 30 + 20 * i);
     }
 
     // fill in the path we defined
     peerReviewCtx.stroke();
 
-    // Draw peer review bars
     $.ajax({
         url: '/peerReview.json',
         dataType: 'json',
         success: function(data) {
-            // full bar
-            // peerReviewCtx.fillRect(30, 30, 5, 200);
-
             var categories = Object.keys(data);
 
             categories.forEach(function(category, index) {
@@ -31,12 +39,13 @@ $(function() {
                 var y = 30 + (10 - value) * 20;
                 var height = value * 20;
 
+                peerReviewCtx.fillStyle = colors[index];
+                // Draw bar
                 peerReviewCtx.fillRect(x, y, 5, height);
-            });
 
-            // Draw labels
-            categories.forEach(function(category, index) {
-                peerReviewCtx.fillText(category, 100, 30 + 20 * index);
+                // Draw label
+                peerReviewCtx.fillRect(100, 80 + 20 * index, 10, 10);
+                peerReviewCtx.fillText(category, 120, 90 + 20 * index);
             });
         }
     });
